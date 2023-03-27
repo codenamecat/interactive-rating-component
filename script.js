e0dc68371784fe3590a665be293302d431a18a96
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-03-27 14:23:58 +0300

    change to radio buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+// variables for elements (sections, each rating button value; not the buttons themselves, submit button)
+
+// function triggered by onclick submit
+  // change one section to display while the other is hidden
+
+// function for when rating button is clicked
+  // when rating button is clicked, replace text element on 2nd card with value of button
+
