fb0af091968c9e02e5975a45252e01f10e46f040
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-03-28 09:50:11 +0300

    basically it works

--- script.js
+++ script.js
@@ -6,3 +6,34 @@
 // function for when rating button is clicked
   // when rating button is clicked, replace text element on 2nd card with value of button
 
+
+// model
+
+const submitBtn = document.getElementById('submit-btn');
+const ratingCard = document.getElementById('rating-card');
+const thankYouCard = document.getElementById('thank-you-card');
+const ratingSelection = document.getElementById('rating-selection');
+
+let selectedOption;
+
+
+// view
+
+submitBtn.addEventListener('click', function() {
+    ratingCard.style = 'display: none';
+    thankYouCard.style = 'display: block';
+
+    displaySelectedOption();
+})
+
+// controller?
+
+function displaySelectedOption() {
+    const ratingOptions = document.getElementsByName('rating');
+    
+    for (let i = 0; i < ratingOptions.length; i++) {
+        if (ratingOptions[i].checked) {
+            ratingSelection.textContent = 'You selected ' + ratingOptions[i].value + ' out of 5';
+        }
+    }
+}
